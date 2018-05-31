160fda30a353f028d86c0efcb4d650c948a96d5f
munenyan1002 <sitaziki@gmail.com>
2018-05-31 22:39:01 +0900

    クエリパラメータを使えるように修正

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -1,10 +1,15 @@
 class HeloController < ApplicationController
   def index
-    msg = '
+    if params['msg'] != nil then
+      msg = 'Hello, ' + params['msg'] + '!'
+    else
+      msg = 'this is sample page.'
+    end
+    html = '
       <html>
       <body>
         <h1>Sample Page</h1>
-        <p>this is Rails sample page!</p>
+        <p>' + msg + '</p>
       </body>
       </html>
     '
