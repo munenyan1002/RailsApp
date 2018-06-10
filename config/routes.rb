01597952201d3d3424aded5a7b610fc98f5dd1a0
munenyan1002 <sitaziki@gmail.com>
2018-06-11 01:12:09 +0900

    伝言ゲームを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'dengonban/index'
+  post 'dengonban/index'
+  get 'dengonban', to: 'dengonban#index'
+  post 'dengonban', to: 'dengonban#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'helo/index'
   get 'helo', to: 'helo#index'
